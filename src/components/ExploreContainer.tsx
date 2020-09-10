1e92c2ab276eb9493c93d03268926c642c0c83ae
rzadp <roopert7@gmail.com>
2020-09-10 14:41:01 +0200

    Initial commit

---
+++ src/components/ExploreContainer.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ExploreContainer.css';
+
+interface ContainerProps {
+  name: string;
+}
+
+const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
+  return (
+    <div className="container">
+      <strong>{name}</strong>
+      <p>Hello world from Przem</p>
+      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    </div>
+  );
+};
+
+export default ExploreContainer;
