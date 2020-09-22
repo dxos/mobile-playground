712f5a4a2d33d3e05bc3c5d090990b4b50a87b5c
Przemyslaw Rzad <przemek@ethworks.io>
2020-09-22 15:44:18 +0200

    APK operations (#2)
    * Import cli app for PoC
    
    * Try the flow
    
    * CI trigger
    
    * Use cli from NPM
    
    * Final touches

---
+++ scripts/publish-apk.js
@@ -0,0 +1,12 @@
+const {publish} = require('@dxos/cli-app/dist/es/handlers/publish')
+
+const config = {
+  get: (key) => {
+    if (key === 'services.ipfs.server') return 'https://apollo1.kube.moon.dxos.network/dxos/ipfs/api'
+    throw new Error(`No config for ${key}`)
+  }
+}
+
+publish(config)({path: '../ipfs-bundle'})
+  .catch(console.error)
+  .then(() => console.log('Done!'))
